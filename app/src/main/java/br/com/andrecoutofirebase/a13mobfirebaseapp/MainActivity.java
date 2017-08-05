39762b3dd05d494fdafe156269572f789e7fd2e5
logonrm <yogacouto@gmail.com>
2017-08-05 15:44:35 -0300

    ok

--- app/src/main/java/br/com/andrecoutofirebase/a13mobfirebaseapp/MainActivity.java
+++ app/src/main/java/br/com/andrecoutofirebase/a13mobfirebaseapp/MainActivity.java
@@ -2,15 +2,30 @@ package br.com.andrecoutofirebase.a13mobfirebaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         FirebaseMessaging.getInstance().subscribeToTopic("mob");
+    }
+
+    public void forcarCrash(View view) {
+       int x = 2 / 0;
+    }
+
+    public void clickMe(View view) {
+        Bundle params = new Bundle();
+        params.putString("NOME","COUTO");
+        mFirebaseAnalytics.logEvent("clickMe", params);
     }
 }
